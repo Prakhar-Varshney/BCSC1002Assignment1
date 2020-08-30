a25f3753409f2a333821f56d906ead8908291e12
Prakhar Varshney <67824412+Prakhar-Varshney@users.noreply.github.com>
2020-08-30 15:40:46 +0530

    add Fields in Library.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -7,4 +7,10 @@
 package definitions;
 
 public class Student {
+    private String[] nameOfStudent;
+    private long universityRollNumber;
+    private int numberOfBooksIssued;
+    private Book[] issuedBooks;
+
+
 }
