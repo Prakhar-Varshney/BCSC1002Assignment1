fe45f2fe2333236c26072f8d0b63cb39af3d3e85
Prakhar Varshney <67824412+Prakhar-Varshney@users.noreply.github.com>
2020-08-30 15:43:56 +0530

    add Final Commit Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -50,5 +50,50 @@ public class Student {
         this.issuedBooks = issuedBooks;
     }
 
+    /**
+     * This method issue book to a student.
+     *
+     * @param issueBook The book to be issued to a student.
+     */
+    public void issueBook(Book issueBook) {
+        System.out.println("Book has been issued to student.");
+    }
+
+    /**
+     * This method checks if the book is present in issued books record of student and returns the issued book back to library
+     *
+     * @param bookToReturn The issued book to be returned.
+     * @return a book to be returned.
+     */
+    public Book returnIssuedBook(String bookToReturn) {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Student Name: " + Arrays.toString(nameOfStudent) +
+                "\nUniversity Roll Number: " + universityRollNumber +
+                "\nNumber Of Books Issued: " + numberOfBooksIssued +
+                "\nIssued Books: " + Arrays.toString(issuedBooks);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return universityRollNumber == student.universityRollNumber &&
+                numberOfBooksIssued == student.numberOfBooksIssued &&
+                Arrays.equals(nameOfStudent, student.nameOfStudent) &&
+                Arrays.equals(issuedBooks, student.issuedBooks);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(universityRollNumber, numberOfBooksIssued);
+        result = 31 * result + Arrays.hashCode(nameOfStudent);
+        result = 31 * result + Arrays.hashCode(issuedBooks);
+        return result;
+    }
 
 }
